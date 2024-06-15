1d8a0b3253b86601570817c3a5fe34eb50ba744a
Fat Han Nuraddin <fat.han.nuraddin@gmail.com>
2024-06-16 01:06:37 +0700

    Remove unecessary files

--- src/Exceptions/PenaException.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace Hanaddi\Pena\Exceptions;
-
-class PenaException extends \Exception {
-    public function __construct($message, $code = 0, \Exception $previous = null)
-    {
-        parent::__construct($message, $code, $previous);
-    }
-}
